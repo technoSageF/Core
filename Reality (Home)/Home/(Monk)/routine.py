76d5c17e4ccf82578a24a64ee66a1fc5cf5e90fd
Fletcher <techno.fletch314@gmail.com>
2024-03-29 17:28:29 +1000

    Full Update

---
+++ Reality (Home)/Home/(Monk)/routine.py
@@ -0,0 +1,31 @@
+class Me:
+    def __init__(self, name):
+        self.name = name
+
+    def breathe(self):
+        print(f"{self.name} is breathing.")
+
+    def look(self):
+        print(f"{self.name} is looking.")
+
+    def think(self):
+        print(f"{self.name} is thinking.")
+
+    def feel(self):
+        print(f"{self.name} is feeling.")
+
+    def talk(self):
+        print(f"{self.name} is talking.")
+
+    def flow(self):
+        print(f"{self.name} is flowing.")
+
+    def vibe(self):
+        print(f"{self.name} is vibing.")
+
+    def be(self):
+        print(f"{self.name} is being.")
+
+    def do(self):
+        print(f"{self.name} is doing.")
+
